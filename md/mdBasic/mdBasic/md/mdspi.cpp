6206785608609eb89c9f08058c1ff216fc9645c7
songxiaoyuan <xiaoyuan@nfs.iscas.ac.cn>
2017-05-19 13:58:12 +0800

    add the mdbasic

---
+++ md/mdBasic/mdBasic/md/mdspi.cpp
@@ -0,0 +1,69 @@
+#include "mdspi.h"
+using namespace std;
+
+
+void CtpMdSpi::OnRspError(CThostFtdcRspInfoField *pRspInfo, int nRequestID,
+                          bool bIsLast) {
+  IsErrorRspInfo(pRspInfo);
+}
+
+void CtpMdSpi::OnFrontDisconnected(int nReason) {
+  cerr << " return | break..."
+       << " reason=" << nReason << endl;
+}
+
+void CtpMdSpi::OnHeartBeatWarning(int nTimeLapse) {
+  cerr << " return | heart warming..."
+       << " TimerLapse = " << nTimeLapse << endl;
+}
+
+void CtpMdSpi::OnFrontConnected() {
+  cerr << " connect md ...success" << endl;
+}
+
+
+void CtpMdSpi::OnRspUserLogin(CThostFtdcRspUserLoginField *pRspUserLogin,
+                              CThostFtdcRspInfoField *pRspInfo, int nRequestID,
+                              bool bIsLast) {
+  if (!IsErrorRspInfo(pRspInfo) && pRspUserLogin) {
+    cerr << " 响应 | 登录成功...当前交易日:" << pRspUserLogin->TradingDay
+         << endl;
+  }
+}
+
+void CtpMdSpi::OnRspSubMarketData(
+    CThostFtdcSpecificInstrumentField *pSpecificInstrument,
+    CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+  cerr << " 响应 |  行情订阅...成功" << endl;
+}
+
+void CtpMdSpi::OnRspUnSubMarketData(
+    CThostFtdcSpecificInstrumentField *pSpecificInstrument,
+    CThostFtdcRspInfoField *pRspInfo, int nRequestID, bool bIsLast) {
+  cerr << " 响应 |  行情取消订阅...成功" << endl;
+}
+
+void CtpMdSpi::OnRtnDepthMarketData(
+    CThostFtdcDepthMarketDataField *pDepthMarketData) {
+  string InstrumentID = pDepthMarketData->InstrumentID;
+  cout<<InstrumentID<<endl;
+  strToFile(InstrumentID);
+
+/*
+  cout << pDepthMarketData->InstrumentID << endl;
+  cout << pDepthMarketData->TradingDay << endl;
+  cout << pDepthMarketData->LastPrice << endl;
+  cout << pDepthMarketData->Volume << endl;
+  cout << pDepthMarketData->BidPrice1 << endl;
+  cout << pDepthMarketData->AskPrice1 << endl;
+  */
+}
+
+bool CtpMdSpi::IsErrorRspInfo(CThostFtdcRspInfoField *pRspInfo)
+{
+  bool ret = ((pRspInfo) && (pRspInfo->ErrorID != 0));
+  if (ret){
+    cerr<<" 响应 | "<<pRspInfo->ErrorMsg<<endl;
+  }
+  return ret;
+}
